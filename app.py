256d0e7422a1e142b87f0e1c663aa1065dda3cec
Hampus-G-B <hampuseliasmikael.bard@studerande.plushogskolan.se>
2026-02-04 13:26:30 +0100

    Initial Wordle Logic Added

--- app.py
+++ app.py
@@ -1,14 +1,33 @@
 import random
 from flask import Flask, json, render_template
+from wtforms.validators import disabled
 
+from models.forms import WordleGuesses
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = 'SECRET_KEY_NON_ENV_FIX_LATER'
 
 with open("data/answer.json") as f:
     words = json.load(f)["words"]
 
 answer=random.choice(words)
+guesses=[]
 print("ANSWER:",answer)
+
+def validate(currentWord, userGuess):
+    result = []
+    word = list(currentWord.lower())
+    guess = list(userGuess.lower())
+    count = 0
+    for letter in guess:
+        if letter == word[count]:
+            result.append('O')
+        elif letter in word:
+            result.append('?')
+        else:
+            result.append('x')
+        count += 1
+    return result
 
 @app.route("/")
 def main():
@@ -19,9 +38,13 @@ def main():
         words=words
     )
 
-@app.route('/wordle')
+@app.route('/wordle', methods=["GET", "POST"])
 def wordle():
-    return render_template('wordle/wordle_guess.html')
+    form = WordleGuesses()
+    if form.validate_on_submit():
+        guesses.append(f"{form.guess.data} | {validate(answer, form.guess.data)}")
+        form.guess.data = ""
+    return render_template('wordle/wordle_guess.html', form = form, guesses=guesses)
 
 if __name__ == '__main__':
     app.run(debug=True)
