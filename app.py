e280c677d60de289a38179e5877aa4824552f644
Hampus-G-B <hampuseliasmikael.bard@studerande.plushogskolan.se>
2026-02-05 10:43:15 +0100

    switched index and wordle

--- app.py
+++ app.py
@@ -30,15 +30,11 @@ def validate(currentWord, userGuess):
 
 @app.route("/")
 def main():
-    return render_template(
-        "index.html",
-        answer=answer,
-        words=words
-    )
+    return render_template('wordle/wordle_guess.html', guesses=guesses)
 
 @app.route('/wordle', methods=["GET", "POST"])
 def wordle():
-    return render_template('wordle/wordle_guess.html', guesses=guesses)
+    pass
 
 if __name__ == '__main__':
     app.run(debug=True)
