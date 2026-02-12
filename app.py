be8017e5b6fc626c3fbe7dc816751755d782504f
Hampus-G-B <hampuseliasmikael.bard@studerande.plushogskolan.se>
2026-02-12 09:15:54 +0100

    implemented Rujikarn allowed word check

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ import random
 from flask import Flask, json, render_template
 
 from models.forms import WordleGuesses
-from validation import validate
+from validation import validate, check_guess_validity
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = 'SECRET_KEY_NON_ENV_FIX_LATER'
 
-with open("data/answer.json") as f:
+with open("data/allow_words.json") as f:
     words = json.load(f)["words"]
 
 answer=random.choice(words)
@@ -18,10 +18,12 @@ print("ANSWER:",answer)
 @app.route("/", methods=["GET", "POST"])
 def main():
     form = WordleGuesses()
-    print(guesses)
-    if form.guess.data is not None:
+
+    if form.guess.data is not None and check_guess_validity(form.guess.data, words)[0] != False:
         guesses.append(validate(answer,form.guess.data))
         form.guess.data = None
+    else:
+        form.guess.data = "Invalid Word"
     return render_template('wordle/wordle_guess.html', form=form, guesses=guesses)
 
 @app.route('/wordle')
